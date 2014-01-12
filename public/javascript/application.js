d4f3cab7d9244ec57863cbdb49d335fdd92038e7
Simon Kiener <jugglinghobo@gmail.com>
2014-01-12 20:48:43 +0100

    added loading animation to gif

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -11,9 +11,17 @@ var Application = function() {
   }
 
   var initChangeListener = function() {
-    $("#new_image_input").on("change", "input", function() {
+    $("#new_image_input").on("keyup", "input", function() {
+      $("#loading").toggle(true);
+      $("#new_image").toggle(false);
       var img_url = $(this).val();
-      $("#new_image").html("<img id="+img_url+" src="+img_url+" />")
+      var loaded_image = $('<img />').load( function(){
+        $("#loading").toggle(false);
+        $("#new_image").html(loaded_image);
+        $("#new_image").toggle(true);
+      }).error(function() {
+        $("#new_image").toggle(false);
+      }).attr('src', img_url);
     });
   }
 
